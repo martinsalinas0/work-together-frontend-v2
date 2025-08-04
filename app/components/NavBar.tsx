337fa68b21b748bda44687da650e0633270f9e81
martinsalinas0 <martinlsalinas@pm.me>
2025-08-04 14:49:28 -0500

    Add NavBar component and update layout; include react-icons dependency

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import React from "react";
+import { IoMdAdd } from "react-icons/io";
+
+const NavBarComponent = () => {
+  return (
+    <nav className="bg-stone-600 text-3xl">
+      <nav className="min-w-auto bg-darkblue shadow-2xl px-6 py-4">
+        <div className="flex justify-between items-center">
+          <div className="flex space-x-6 ">
+            <p className=" hover:text-orange-300 px-3 py-2 rounded-md text-sm font-medium transition-colors">
+              <Link href="/">Home</Link>
+            </p>
+            <p className=" hover:text-orange-300 px-3 py-2 rounded-md text-sm font-medium transition-colors">
+              <Link href="/jobs">Jobs</Link>
+            </p>
+            <p className=" hover:text-orange-300 px-3 py-2 rounded-md text-sm font-medium transition-colors">
+              <Link href="/settings">Settings</Link>
+            </p>
+            <p className=" hover:text-orange-300 px-3 py-2 rounded-md text-sm font-medium transition-colors">
+              <Link href="/jobs-test">jobs test</Link>
+            </p>
+          </div>
+          <div className="px-3 py-2 hover:text-blue-300 flex items-center space-x-2">
+            <Link href="/jobs/new" className="flex items-center space-x-1">
+              <span className="text-sm sm:text-base">new job</span>
+              <IoMdAdd className="text-lg sm:text-xl" />
+            </Link>
+          </div>
+          <div className=" font-bold px-3 py-2 hover:text-blue-300">
+            <Link href="/user">User</Link>{" "}
+          </div>
+        </div>
+      </nav>
+    </nav>
+  );
+};
+
+export default NavBarComponent;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import NavBarComponent from "./components/NavBar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,6 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <NavBarComponent />
       <body>{children}</body>
     </html>
   );
